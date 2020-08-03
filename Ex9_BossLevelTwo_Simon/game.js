260c7eb56e0dd6bb43c22ae19cbf623e39fdfdbf
AkhilChak <61554464+AkhilChak@users.noreply.github.com>
2020-08-04 04:28:24 +0530

    Add files via upload

---
+++ Ex9_BossLevelTwo_Simon/game.js
@@ -0,0 +1,79 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var gameStarted = false;
+
+function nextSequence() {
+    level++;
+    $("h1").text("Level " + level);
+    userClickedPattern = [];
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour)
+        .fadeIn(100)
+        .fadeOut(100)
+        .fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        console.log("success");
+        if (currentLevel + 1 === level) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
+}
+
+// EFFECTS
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+// Detecting Inputs
+
+$(".btn").click(function () {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).keypress(function (event) {
+    if (gameStarted == false) {
+        setTimeout(() => {
+            nextSequence();
+        }, 500);
+
+        gameStarted = true;
+    }
+});
